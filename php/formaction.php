75ced0c3f265b7b5a110f03419c75807b2eae29f
Nakaryakova <nakaryakova.julia@mail.ru>
2016-04-25 22:48:52 +0500

    3 лабораторная

---
+++ php/formaction.php
@@ -0,0 +1,68 @@
+<?php 
+header("Content-Type: text/html; charset=utf-8"); 
+session_start();
+	//Проверка сессии
+	if ($_SESSION['test'] == md5($_SERVER['REMOTE_ADDR']))  
+	{
+	//Сообщение, которое вернется пользователю
+	$_SESSION['message']='';
+	//Сюда запишем данные из формы
+	$Ail = $Name = $Tel = '';
+	$NewLine = '--------------';
+	if(check_length($_GET["Name"], 2, 16) || check_length($_GET["Ail"], 2, 15) || check_length($_GET["Tel"], 2, 15))
+	{
+		$_SESSION['message'] = 'Проверьте правильность введенных данных!';
+	}
+	else
+	{
+		$Ail = clean($_GET["Ail"]);
+		$Name = clean($_GET["Name"]);
+		$Tel = clean($_GET["Tel"]);		
+		//Открываем файл
+		$fp  = fopen('commentfile.txt', 'a+'); 
+		//Записываем построчно каждое значение из полей формы
+		fwrite($fp, $NewLine."\n");
+		fwrite($fp, $Name."\n"); 		
+		fwrite($fp, $Tel."\n"); 	
+		fwrite($fp, $Ail."\n"); 		
+		//Закрываем файл
+		fclose($fp); 
+		$_SESSION['message'] = $Name.' Ваша запись успешно занесена в базу, мы вам обязательно перезвоним на ваш номер - '.$Tel;
+	}
+	}
+	else
+	{
+		$_SESSION['message'] = 'Доступ закрыт.';
+	}
+	back();
+?>
+
+
+<?php 
+function back() 
+{
+echo "
+<html>
+  <head>
+   <meta http-equiv='Refresh' content='0; URL=".$_SERVER['HTTP_REFERER']."'>
+  </head>
+</html>";
+}
+?>
+
+<?php 
+function clean($value = "") {
+    $value = trim($value);
+    $value = stripslashes($value);
+    $value = strip_tags($value);
+    $value = htmlspecialchars($value);    
+    return $value;
+}
+?>
+
+<?php
+function check_length($value = "", $min, $max) {
+    $result = (mb_strlen($value) < $min || mb_strlen($value) > $max);
+    return $result;
+}
+?>
